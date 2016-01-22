8c84fb7c925daf8748f4648669e386c20bc8d4a9
Daniel Lobato <elobatocs@gmail.com>
2016-01-22 18:01:25 +0100

    Fixes #13333 - Use Patternfly cards to redesign job invocations
    It's a bit difficult to notice some sections in the job invocations
    page. Similarly, the piechart is strangely aligned to the right with a
    big space in between.
    To try to overcome these issues and homogenize the interface, we can use
    Patternfly cards
    https://rawgit.com/patternfly/patternfly/master/tests/cards.html
    
    This is how it used to look like:
    http://theforeman.org/plugins/foreman_remote_execution/0.1/job_detail_1.png
    After this change, it looks like: http://i.imgur.com/3kKigMr.png
    
    React Implementation

--- app/helpers/remote_execution_helper.rb
+++ app/helpers/remote_execution_helper.rb
@@ -77,9 +77,16 @@ module RemoteExecutionHelper
   end
 
   def template_invocation_actions(task, host, job_invocation, template_invocation)
+    host_task = template_invocation.try(:run_host_job_task)
     [
       display_link_if_authorized(_('Host detail'), hash_for_host_path(host).merge(:auth_object => host, :permission => :view_hosts, :authorizer => job_hosts_authorizer)),
       display_link_if_authorized(_('Rerun on %s') % host.name, hash_for_rerun_job_invocation_path(:id => job_invocation, :host_ids => [ host.id ], :authorizer => job_hosts_authorizer)),
+      if host_task.present?
+        display_link_if_authorized(
+          _('Host task'),
+          hash_for_foreman_tasks_task_path(host_task).
+          merge(:auth_object => host_task, :permission => :view_foreman_tasks))
+      end
     ]
   end
 
@@ -130,6 +137,7 @@ module RemoteExecutionHelper
     end
     return buttons
   end
+
   # rubocop:enable Metrics/AbcSize
 
   def template_invocation_task_buttons(task)
@@ -151,7 +159,7 @@ module RemoteExecutionHelper
                          :disabled => !task.cancellable?,
                          :method => :post)
     end
-    return buttons
+    buttons
   end
 
   def link_to_invocation_task_if_authorized(invocation)
@@ -197,17 +205,6 @@ module RemoteExecutionHelper
     else
       alert :class => 'alert-block alert-danger base in fade has-error',
             :text => renderer.error_message.html_safe # rubocop:disable Rails/OutputSafety
-    end
-  end
-
-  def job_invocation_active_tab(tab, params)
-    active = 'active'
-    inactive = ''
-    hosts_tab_active = params[:page].present? || params[:search].present? || params[:order].present?
-    if hosts_tab_active
-      tab == :hosts ? active : inactive
-    else
-      tab == :overview ? active : inactive
     end
   end
 
---
+++ webpack/react_app/redux/reducers/job_invocations/index.js
@@ -0,0 +1,27 @@
+import Immutable from 'seamless-immutable';
+
+import {
+  JOB_INVOCATIONS_GET_JOB_INVOCATIONS,
+  JOB_INVOCATIONS_POLLING_STARTED,
+} from '../../consts';
+
+const initialState = Immutable({
+  isPolling: false,
+  job_invocations: [],
+  statuses: []
+});
+
+export default (state = initialState, action) => {
+  const { payload } = action;
+
+  switch (action.type) {
+    case JOB_INVOCATIONS_POLLING_STARTED:
+      return state.set('isPolling', true);
+    case JOB_INVOCATIONS_GET_JOB_INVOCATIONS:
+      return state
+        .set('job_invocations', payload.jobInvocations.job_invocations)
+        .set('statuses', payload.jobInvocations.statuses);
+    default:
+      return state;
+  }
+};
